cc4fc7ea7c8ea4cf664a8d75ef3846867309a253
lucas1969 <romanoabdolucas@gmail.com>
2025-03-19 19:47:10 -0300

    cargar instrucción, enmascarar y comparar

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -2,10 +2,40 @@
 #include <assert.h>
 #include <string.h>
 
+#include<stdint.h>
+#include "shell.h"
+
+extern CPU_State CURRENT_STATE;
+
+/*
+se puede modelar como punteros a función. 
+no importa el orden en el que se empieza a comparar. 
+pasar parámetros a main. 
+*/
+
+
 void process_instruction()
 {
     /* execute one instruction here. You should use CURRENT_STATE and modify
-     * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
-     * access memory. 
-     * */
+    * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
+    * access memory. 
+    * */
+    uint64_t PC = CURRENT_STATE.PC;
+    uint32_t instruction =  mem_read_32(PC);
+
+    //Para chequear que estamos leyendo
+    printf("Instruction: %x\n", instruction);
+
+    int adds_opcode = 0b10110001<<24;
+    int adds_mask = 0b11111111<<24;
+
+    printf("opcode: %x\n", adds_opcode);
+
+    // printf("Mask: %n", instruction & adds_mask);
+
+    if ((instruction & adds_mask) == adds_opcode) {
+        printf("es un ADDS!!!!\n");
+    } else {
+        printf("no es un ADDS\n");
+    }
 }
