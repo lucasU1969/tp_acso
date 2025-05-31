326737fbcbdc1538f9ed49f46a72ad843074b03a
lucasU1969 <lromanoabdo@udesa.edu.ar>
2025-05-31 19:54:45 -0300

    refactor y extracción de funciones en el ejercicio 1

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -6,8 +6,13 @@
 #include <string.h>
 
 
-int main(int argc, char **argv)
-{	
+// FALTAN CHEQUEOS
+
+void write_int_pipe(int fd, const int *buff);
+void read_int_pipe(int fd, int *buff);
+void close_pipes(int fds[][2], int read, int write, int n);
+
+int main(int argc, char **argv) {	
 	int start, status, pid, n;
 	int buffer[1];
 
@@ -23,48 +28,69 @@ int main(int argc, char **argv)
     
    	/* You should start programming from here... */				
 
-	int fds[n][2];  									// Tengo que crear n pipes para comunicar entre todos los procesosos. 
+	int fds[n+1][2];  														// Tengo que crear n+1 pipes para comunicar entre todos los procesos. 
 	for (int i = 0; i <= n; i++) {
-		pipe(fds[i]);									// creo los pipes para la comunicación entre los procesos.
+		pipe(fds[i]);														// creo los pipes para la comunicación entre los procesos.
 	}
 	
-	pid_t child_pid[n]; 								// para guardar los pids de los hijos.
+	pid_t child_pid[n]; 													// para guardar los pids de los hijos.
 	int child_idx;
 	for (int i = 0; i < n; i++) {
-		child_pid[(i-start)%n] = fork(); 				// relleno el vector de pids de los hijos.
-		int child_idx = (i-start)%n;
-		if (!child_pid[i]) break;						// soy un hijo, entonces tengo que dejar de ejecutar el código porque no quiero seguir creando procesos.
+		pid = fork();
+		child_idx = (i - start + n) % n;									// se suma n porque en C la operación % puede devolver negativos (?)
+		child_pid[child_idx] = pid;
+		if (!pid) break;													// soy un hijo, entonces tengo que salir del while para no crear más procesos.
 	}
 
-	if (child_pid[child_idx]) {							// soy el padre
-		
-		for (int i = 0; i < n; i++) {					// cerrar todos los pipes que no voy a usar.
-			if (i==0) {
-				close(fds[i][0]); 
-			} else if (i==n-1) {
-				close(fds[i][1]);
-			} else {
-				close(fds[i][0]); 
-				close(fds[i][1]);
-			}
+	if (pid) {																// soy el padre
+		close_pipes(fds, n, 0, n+1);										// cierro los pipes que no voy a usar
+		write_int_pipe(fds[0][1], buffer);									// paso el dato al hijo que corresponde
 
+		int ret[1]; 
+		read_int_pipe(fds[n][0], ret); 										// leo el dato que me devuelve el último hijo
+
+		printf("%d\n", ret[0]);	
+
+		for (int i = 0; i < n; i++) waitpid(child_pid[i], &status, 0);		// espero a los hijos para liberar recursos
+
+	} else {																// soy hijo
+		close_pipes(fds, child_idx, child_idx+1, n+1);						// cierro los pipes que no voy a usar
+
+		int c[1];
+		read_int_pipe(fds[child_idx][0], c);								// leo el dato que me pasa el hijo anterior
+
+		c[0] = c[0] + 1;
+		write_int_pipe(fds[child_idx+1][1], c);								// paso el dato incrementado al próximo hijo
+	}
+	return 0;
+}
+
+
+void close_pipes(int fds[][2], int read, int write, int n) {
+	for (int i = 0; i<n; i++) {
+		if (i==read) {
+			close(fds[i][1]);
+		} else if (i==write) {
+			close(fds[i][0]);
+		} else {
+			close(fds[i][0]);
+			close(fds[i][1]); 
 		}
-
-		write(fds[0][0], buffer, sizeof(buffer));		// esto puede dar error si es negativo -> hacer el chequeo
-
-		// tengo que escribir c en el pipe que conecta con el hijo con child_idx = 0 
-
-		// escuchar en el último pipe que es en el que voy a recibir el dato despues de que el último hijo lo incremente.
-
 	}
+}
 
-	else {
-		// calcular el índice del pipe en el que tengo que escuchar
-		// cerrar todos los pipes de los que no voy a escuchar.
-		// incrementar el dato en 1
-		// escribir el dato en el pipe siguiente al usado para escuchar
+void read_int_pipe(int fd, int *buff) {
+	if (read(fd, buff, sizeof(int)) < 0) {
+		perror("read"); 
+		exit(EXIT_FAILURE); 
 	}
+	close(fd);
+}
 
-
-
+void write_int_pipe(int fd, const int *buff) {
+	if (write(fd, buff, sizeof(int)) < 0) {
+		perror("write"); 
+		exit(EXIT_FAILURE); 
+	}
+	close(fd);
 }
